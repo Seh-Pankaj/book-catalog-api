a1f952defef97990b6493193c74bb5fe5fc5592b
Seh-Pankaj <pankajsehrawat.605@gmail.com>
2025-09-14 19:42:26 +0530

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./db/connection");
+const cors = require("cors");
 const bookRoutes = require("./routes/bookRoutes");
 const userRoutes = require("./routes/userRoutes");
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/books", bookRoutes);

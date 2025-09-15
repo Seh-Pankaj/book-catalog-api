30e84e6af833cfc4d801de040914f47c4e8f9c8a
Seh-Pankaj <pankajsehrawat.605@gmail.com>
2025-09-15 18:11:35 +0530

    fix: delete api improved

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -57,6 +57,7 @@ const deleteBookRecord = async (req, res) => {
     const id = req.params.id;
     if (!id || id == ":id") throw Error("Id is required!");
     const deletedRecord = await Book.findByIdAndDelete(id);
+    if (!deletedRecord) throw Error("Book Id absent!");
     res.status(204).end();
   } catch (error) {
     res.status(400).json({ error: error.message });

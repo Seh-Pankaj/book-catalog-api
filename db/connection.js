2e7fa8403bd38e21c9322866cd2eb4bfed7993de
Seh-Pankaj <pankajsehrawat.605@gmail.com>
2025-09-15 17:13:18 +0530

    fix: db connection cached

--- db/connection.js
+++ db/connection.js
@@ -5,10 +5,18 @@ const PASS = process.env.PASS || "filler";
 
 const uri = `mongodb+srv://${USER}:${PASS}@maindb.savq0je.mongodb.net/booksDB`;
 
+let isConnected;
+
 const connectDB = async () => {
+  if (isConnected) {
+    console.log("DB already connected");
+    return;
+  }
+
   try {
     const db = await mongoose.connect(uri);
     console.log("Connection to DB successful");
+    isConnected = db.connections[0].readyState;
   } catch (error) {
     console.log("Connection to DB failed", error);
   }
